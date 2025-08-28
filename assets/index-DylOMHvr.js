72d59704e62b601fbecf0db9f2d56e943c8dccb3
MarchettoFrancesco <silver2002.fm@gmail.com>
2025-08-28 18:47:02 +0200

    testo aggiunto descrizione

--- assets/index-DylOMHvr.js
+++ assets/index-DylOMHvr.js
@@ -16831,7 +16831,8 @@ function dS() {
                                   className: "text-blue-500 underline",
                                   children: "Agrichange",
                                 }),
-                                ". Now we will partecipate in the Enactus World Cup in Thailand. I care about health and overall wellness, and I’m also deeply into music and fashion, which inspire me and fuel my creativity.",
+                                ". Now we will partecipate in the Enactus World Cup in Thailand.",
+                                " I care about health and overall wellness, and I’m also deeply into music and fashion, which inspire me and fuel my creativity.",
                               ],
                             }),
                             C.jsxs("div", {
